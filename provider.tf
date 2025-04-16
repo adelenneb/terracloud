59580c4a30e8045b138d107e61f952421b4931f4
adel <43046987+adelenneb@users.noreply.github.com>
2025-04-16 14:41:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
 }
 resource "azurerm_resource_group" "examplecoud" {
-  name     = "rg1-adel-cloud"
+  name     = "rg1-adel-cloud20"
   location = "westus"
 }
