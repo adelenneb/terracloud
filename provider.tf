44f07bcae12db857c6f9fb7d90c0aa9aa35362a2
adelenneb <enneb.adel@gmail.com>
2025-04-16 13:37:39 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {
+  }
+subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
+}
+resource "azurerm_resource_group" "examplecoud" {
+  name     = "rg1-adel-cloud"
+  location = "westus"
+}
